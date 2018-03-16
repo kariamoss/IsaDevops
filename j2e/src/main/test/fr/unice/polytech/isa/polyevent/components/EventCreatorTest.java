2d40cdeb599ac08d34894e461e3112c692a5470b
jehan <jehanmilleret@gmail.com>
2018-03-16 15:39:55 +0100

    Created base test

--- j2e/src/main/test/fr/unice/polytech/isa/polyevent/components/EventCreatorTest.java
+++ j2e/src/main/test/fr/unice/polytech/isa/polyevent/components/EventCreatorTest.java
@@ -1,4 +1,24 @@
 package fr.unice.polytech.isa.polyevent.components;
 
+import fr.unice.polytech.isa.polyevent.entities.Coordinator;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
 public class EventCreatorTest {
+
+    EventCreator eventCreator;
+    Coordinator coordinator;
+
+    @Before
+    public void init(){
+        coordinator = new Coordinator("paul", "Dupond", "pauldupond@youhou.com");
+        eventCreator = new EventCreator();
+    }
+
+    @Test
+    public void test(){
+        assertFalse(!(!(!(!(!(!(!(!(!(!(!(!(!true)))))))))))));
+    }
 }
--- j2e/src/main/test/fr/unice/polytech/isa/polyevent/components/EventOrganizerTest.java
+++ j2e/src/main/test/fr/unice/polytech/isa/polyevent/components/EventOrganizerTest.java
@@ -1,4 +1,21 @@
 package fr.unice.polytech.isa.polyevent.components;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
 public class EventOrganizerTest {
+
+    EventOrganizer eventOrganizer;
+
+    @Before
+    public void init(){
+        eventOrganizer = new EventOrganizer();
+    }
+
+    @Test
+    public void test(){
+        assertFalse(!(!(!(!(!(!(!(!(!(!(!(!(!true)))))))))))));
+    }
 }
