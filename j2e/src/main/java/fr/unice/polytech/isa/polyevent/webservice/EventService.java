132719f1229e9dbcd89f086af6660d03c8f94e94
maxim <maxime.flament@etu.unice.fr>
2018-03-16 16:52:21 +0100

    Deleted old src/ hierarchy since we modularized all components for the MVP

--- j2e/src/main/java/fr/unice/polytech/isa/polyevent/webservice/EventService.java
+++
@@ -1,9 +0,0 @@
-package fr.unice.polytech.isa.polyevent.webservice;
-
-public class EventService implements IEventService {
-
-    @Override
-    public void createEvent(String name) {
-        System.out.println(name);
-    }
-}
