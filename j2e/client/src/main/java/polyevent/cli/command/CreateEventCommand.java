98f31b4d33a5df51b70f21274fbbde710b46915d
jehan <jehanmilleret@gmail.com>
2018-03-22 11:55:07 +0100

    Corrected createEvent for client

--- j2e/client/src/main/java/polyevent/cli/command/CreateEventCommand.java
+++ j2e/client/src/main/java/polyevent/cli/command/CreateEventCommand.java
@@ -4,9 +4,10 @@ import polyevent.Event;
 import polyevent.RoomType;
 import api.EventApi;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.util.*;
 
 /**
  * The class that will represent the command
@@ -22,6 +23,9 @@ import java.util.List;
 public class CreateEventCommand extends AbstractCommand<EventApi> {
 
     private String eventName;
+    private String email;
+    private XMLGregorianCalendar dateBegin;
+    private XMLGregorianCalendar dateEnd;
     private int estimatedPeopleNumber;
     private List<RoomType> rooms;
 
@@ -37,8 +41,9 @@ public class CreateEventCommand extends AbstractCommand<EventApi> {
 
     @Override
     public void execute() throws Exception {
-        String result = shell.api.eventService.createEvent(eventName);
-        System.out.println("Created event with name: " + result);
+
+        boolean result = shell.api.eventService.createEvent(eventName, estimatedPeopleNumber, dateBegin, email);
+        System.out.println("Completion of event creation: " + result);
     }
 
     /**
@@ -51,6 +56,18 @@ public class CreateEventCommand extends AbstractCommand<EventApi> {
     @Override
     public void load(List<String> args) {
         eventName = args.get(0);
+        estimatedPeopleNumber = Integer.parseInt(args.get(1));
+        email = args.get(2);
+        GregorianCalendar c = new GregorianCalendar();
+        c.setTime(new Date(2018, 10 , 10, 10, 15));
+        // TODO Change to Calendar
+        dateBegin = null;
+        try {
+            dateBegin = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+        } catch (DatatypeConfigurationException e) {
+            e.printStackTrace();
+        }
+
         /*
         TODO keep this code here as it will be used after the MVP development
         estimatedPeopleNumber = Integer.parseInt(args.get(1));
@@ -70,6 +87,7 @@ public class CreateEventCommand extends AbstractCommand<EventApi> {
      */
     @Override
     public String helper() {
-        return "Creates a new event at Polytech.\nUsage: createEvent eventName peopleNumber roomType1 [roomTypes] ";
+        return "Creates a new event at Polytech.\nUsage: createEvent eventName peopleNumber email ";
+        //return "Creates a new event at Polytech.\nUsage: createEvent eventName peopleNumber roomType1 [roomTypes] ";
     }
 }
