753d4dfdbb311af2789f657f6fedd01e9c80da22
maxim <maxime.flament@etu.unice.fr>
2018-03-23 11:24:40 +0100

    added arquillian runwith

--- j2e/event-creator/src/test/java/polyevent/EventCreatorTest.java
+++ j2e/event-creator/src/test/java/polyevent/EventCreatorTest.java
@@ -1,18 +1,31 @@
 package polyevent;
 
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 
 import javax.ejb.EJB;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(Arquillian.class)
 public class EventCreatorTest {
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addPackage(IEventCreator.class.getPackage());
+    }
 
     @EJB
     private IEventCreator eventCreator;
