b2d50e19fcae4ec4f7ff8a7610da2b98b85a8156
Lucas <lucas.matteo@etu.unice.fr>
2018-03-23 09:43:06 +0100

    adding test

---
+++ j2e/core/src/test/java/polyevent/EventTest.java
@@ -0,0 +1,25 @@
+package polyevent;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+public class EventTest {
+    private Event event;
+
+    @Before
+    public void init(){
+        this.event = new Event(100,"salut",new ArrayList<RoomType>());
+    }
+
+    @Test
+    public void roomAddTest(){
+        Room room = new Room(RoomType.MEETING_ROOM,100,"test");
+        this.event.addRoom(room);
+        assertTrue(this.event.getRooms().contains(room));
+    }
+}
