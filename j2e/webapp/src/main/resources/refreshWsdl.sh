02c30a5a0b083c83be6637ab0b23ad3ae227c400
Maxime Flament <maxime.flament@etu.unice.fr>
2018-03-23 11:39:16 -0400

    Updated wsdl refresh

--- j2e/webapp/src/main/resources/refreshWsdl.sh
+++ j2e/webapp/src/main/resources/refreshWsdl.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget http://localhost:8080/polyevent-backend/EventServiceService?wsdl
-mv EventServiceService\?wsdl EventService.wsdl
+wget http://localhost:8080/polyevent-backend/webservices/EventService?wsdl
+mv EventService\?wsdl EventService.wsdl
