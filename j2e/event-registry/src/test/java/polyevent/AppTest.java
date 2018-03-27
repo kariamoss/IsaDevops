51f6e606c574f08e91029e5b08292bd4b425a925
maxim <maxime.flament@etu.unice.fr>
2018-03-27 13:34:53 +0200

    Event registry module

---
+++ j2e/event-registry/src/test/java/polyevent/AppTest.java
@@ -0,0 +1,12 @@
+package polyevent;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void appTest() {
+        assertTrue(true);
+    }
+}
