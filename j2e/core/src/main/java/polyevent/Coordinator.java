61ca47a2cb36ca007900ec882b89d953a0446a1d
Lucas <lucas.matteo@etu.unice.fr>
2018-03-30 08:43:09 +0200

    pom

--- j2e/core/src/main/java/polyevent/Coordinator.java
+++ j2e/core/src/main/java/polyevent/Coordinator.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Coordinator implements Serializable {
     private String firstName;
     private String lastName;
-    private Str email;
+    private String email;
     private List<Event> eventsCreated;
 
     public Coordinator() {
