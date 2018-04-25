471a50cd79a70298184562468bcfa47fa21ef993
jehan <jehanmilleret@gmail.com>
2018-04-25 16:46:02 +0200

    Added latest war script download Changed DockerFile to use the script

---
+++ docker/j2e-docker/latest-war.sh
@@ -0,0 +1,17 @@
+# Artifactory location
+server=http://207.154.199.162:8081/artifactory
+repo=libs-snapshot-local
+
+# Maven artifact location
+name=j2e-interface
+artifact=polyevent/${name}
+path=$server/$repo/$artifact
+version=`curl -s $path/maven-metadata.xml | grep latest | sed "s/.*<latest>\([^<]*\)<\/latest>.*/\1/"`
+build=`curl -s $path/$version/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/"`
+war=$name-$build.war
+url=$path/$version/$war
+
+# Download
+echo $url
+wget -q $url
+mv $war ./apache-tomee-plume-7.0.2/webapps
