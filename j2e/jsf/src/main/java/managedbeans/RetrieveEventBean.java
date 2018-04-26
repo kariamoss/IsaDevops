4f912a496940de1380a0d567532db0c2de1d12e8
jehan <jehanmilleret@gmail.com>
2018-04-26 18:11:40 +0200

    Reset of branch Updated wsdl

---
+++ j2e/jsf/src/main/java/managedbeans/RetrieveEventBean.java
@@ -0,0 +1,22 @@
+package managedbeans;
+
+import polyevent.entities.Event;
+import polyevent.EventCatalog;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Retrieve information from our database, regarding events in particular
+ */
+@ManagedBean
+public class RetrieveEventBean {
+
+    @EJB private EventCatalog eventCatalog;
+
+    public Optional<List<Event>> getEvents(){
+        return eventCatalog.getAllEvents();
+    }
+}
