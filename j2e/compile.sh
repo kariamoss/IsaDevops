7ce1e6adf3006990810d63bb72b7c5dd0ab64fb0
Théo Frasquet <theo.frasquet@gmail.com>
2018-05-01 17:47:31 +0200

    Added premium things

--- j2e/compile.sh
+++ j2e/compile.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-file="core
+files="core
      webapp room-booker event-registry coordinator
-     client event-organizer
+     client event-organizer-root event-organizer-free event-organizer
      event-creator
      j2e-interface"
 
@@ -10,10 +10,9 @@ cd root
 mvn install
 cd ..
 
-for i in $file
+for i in ${files}
 do
-    cd $i
-
+    cd ${i}
     mvn clean install -o
     cd ..
 done
