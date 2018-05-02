59c96439f0c82ddcd56e0c35677cc38d604021be
maxim <maxime.flament@etu.unice.fr>
2018-05-02 16:53:51 +0200

    updated display of events

--- j2e/client/src/main/java/polyevent/cli/command/GetEventWithNameCommand.java
+++ j2e/client/src/main/java/polyevent/cli/command/GetEventWithNameCommand.java
@@ -32,7 +32,7 @@ public class GetEventWithNameCommand extends AbstractCommand<EventApi> {
 
         // no events with this name have been found...
         if (event == null) {
-            System.out.println("No event with the name : " + eventName + " exists in the PolyEvent system!");
+            System.out.println("No event with the name " + eventName + " exists in the PolyEvent system!");
         }
         else {
             System.out.println("Found an event with the name " + eventName + ".");
