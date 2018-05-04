d56014f11c10c396ef44a04e7d697c9d12bdd21b
jehan <jehanmilleret@gmail.com>
2018-05-04 13:02:55 +0200

    Simpler dates

--- j2e/bill-creator/src/main/java/polyevent/Mailing.java
+++ j2e/bill-creator/src/main/java/polyevent/Mailing.java
@@ -22,13 +22,13 @@ public class Mailing {
     public JsonNode sendSimpleMessage() throws UnirestException {
 
         String subject = coordinator.getFirstName() + " " + coordinator.getLastName() + ", voici votre facture";
-        String content = "Votre événement " + event.getName() + " a bien été créé. Votre salle va vous être communiquer prochainement." +
-                "Vous pourrez y accéder du " + event.getStartDate() + " au " + event.getEndDate() +
-                ". La facture pour cet événement a donc bien été établie.";
+        String content = "Votre événement " + event.getName() + " a bien été créé. Votre salle va vous être communiquer prochainement.\n" +
+                "Vous pourrez y accéder du " + event.getStartDate().toLocaleString() + " au " + event.getEndDate().toLocaleString() +
+                ".\nLa facture pour cet événement a donc bien été établie.";
 
         HttpResponse<JsonNode> request = Unirest.post("https://api.mailgun.net/v3/sandboxd67ad01d73c64b15915a5bac7c743c79.mailgun.org/messages")
                 .basicAuth("api", "key-0c2fcad4200b9f1143cd1cce37e01c58")
-                .queryString("from", "Mailgun Sandbox <postmaster@sandboxd67ad01d73c64b15915a5bac7c743c79.mailgun.org>")
+                .queryString("from", "PolyEvent <postmaster@sandboxd67ad01d73c64b15915a5bac7c743c79.mailgun.org>")
                 .queryString("to", coordinator.getFirstName() + " " + coordinator.getLastName() + " <jehanmilleret@gmail.com>")
                 .queryString("subject", subject)
                 .queryString("text", content)
